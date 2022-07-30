1f851199a1b2ee5ea28ed638c57954e3e6d366b2
Prashansa Meyn <pm@globalpay.dk>
2022-07-30 02:21:58 +0200

    SerializableContactNumber added here

---
+++ SerializableContactNumber.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace UmbracoMongoSmsDataServices
+{
+	public class SerializableContactNumber
+	{
+		[JsonProperty("CountryCodeAndPhoneCode")]
+		public string CountryCodeAndPhoneCode { get; set; }
+		[JsonProperty("ContactNumber")]
+		public string ContactNumber { get; set; }
+
+		public string FormattedContactNumber(char sepratorChar)
+		{
+			if (string.IsNullOrWhiteSpace(ContactNumber) || string.IsNullOrWhiteSpace(CountryCodeAndPhoneCode))
+			{
+				return "Error: Incorrect phone number";
+			}
+			return "+" + CountryCodeAndPhoneCode?.Split('#')?.LastOrDefault() + sepratorChar + Regex.Replace(ContactNumber, @"\s+", "");
+		}
+	}
+}
